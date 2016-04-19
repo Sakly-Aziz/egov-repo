80e2d25877c6e634d811740e70434ef2ec0252bf
ines <ineszr@hotmail.fr>
2016-04-19 04:30:17 +0200

    Signed-off-by: ines <ineszr@hotmail.fr>

---
+++ e-gov/e-gov-ejb/src/main/java/egov/services/interfaces/ICaseManagementLocal.java
@@ -0,0 +1,25 @@
+package egov.services.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import egov.entities.Kase;
+
+@Local
+public interface ICaseManagementLocal {
+	
+		Boolean addCase(Kase c);
+
+		Boolean update(Kase c);
+		
+		void flush();
+		
+		Boolean remove(Kase c);
+
+		List<Kase> findAll();
+
+		Kase findCaseById(int id);
+	}
+
+
